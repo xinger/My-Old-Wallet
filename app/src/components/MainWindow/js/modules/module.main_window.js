7b0f86d2ff730b3cf46f243a49b45be526b047e9
Mika <xinger@ya.ru>
2017-12-12 04:07:52 +0300

    some more

---
+++ app/src/components/MainWindow/js/modules/module.main_window.js
@@ -0,0 +1,16 @@
+Air.define('module.main_window', 'lib.DOM', function($) {
+    var self = this;
+
+    self.init = function() {
+        _log('aaaaaa')
+    };
+
+    self.refresh = function() {
+
+    };
+
+    self.destroy = function() {
+
+    };
+
+});
