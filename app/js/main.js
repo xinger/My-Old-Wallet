61dd062d50d73313b5992bcca88afeccea48df94
Mika <xinger@ya.ru>
2017-12-12 03:08:53 +0300

    Initial

---
+++ app/js/main.js
@@ -0,0 +1,16 @@
+window._log = console.log.bind( console );
+
+Air.config( {
+	'variables': {
+		'global_modules': ''
+	},
+	'router': {
+
+	},
+	'global': '{{global_modules}}', // переменная – просто так, для демонстрации
+	'modules': {
+
+	}
+} );
+
+Air.start();
